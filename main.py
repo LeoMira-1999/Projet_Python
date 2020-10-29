4f8add78dd0a28b03db1b57ac8f65da982dc3e4a
savabarde06 <mirandolaleonardo1999@gmail.com>
2020-10-29 14:26:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def printer(arg):
+    x = arg**2
+    for i in range(10):
+        print(x*i)
+
+
+a = printer(2)
+
+print(a)
+
 test
-test
-true
-ALEX
-atom://teletype/portal/66130c0e-9cce-484b-8c9b-c7dcd2fc3c23
