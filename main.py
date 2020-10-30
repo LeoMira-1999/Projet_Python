e13b56ee6d7c7522e6c0bf37a9d2d520515cc888
savabarde06 <you@example.com>
2020-10-30 17:52:33 +0100

    t

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-def printer(arg):
-    x = arg**2
-    for i in range(10):
-        print(x*i)
-
-
-a = printer(100)
-
-sudo ln /usr/bin/python3 /usr/bin/python
-
-atom://teletype/portal/98c69925-fab5-4886-9e6d-6210f0b53fe4
+https://www.ncbi.nlm.nih.gov/books/NBK52640/
