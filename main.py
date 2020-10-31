abc7c0297552cd91f4ec19dc048b476255b17cad
savabarde06 <you@example.com>
2020-10-31 15:37:45 +0100

    zaza

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit"
 
 y = os.system(S)
-print("---------------------------")
+print("--------------------------")
 print(type(y))
 
 for x in y:
