703691c78326dc272a86d99991e8090a978aa0c0
savabarde06 <you@example.com>
2020-10-31 16:25:47 +0100

    a

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-from Bio.Blast import NCBIWWW
-help(NCBIWWW.qblast)
-fasta_string1 = open("GCF_001402945.1_ASM140294v1_genomic.fna").read()
-fasta_string2 = open("GCF_001402935.1_ASM140293v1_genomic.fna").read()
-result_handle = NCBIWWW.qblast("blastn", database = "nr"  ,sequence=fasta_string1)
+import os
+
+S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit"
+
+y = os.system(S)
+print("--------------------------")
+print(type(y))
+
+for x in y:
+    print(x)
