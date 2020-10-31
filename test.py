7e0b5a51f7d358372fa087f66cf2720d556feb8c
savabarde06 <you@example.com>
2020-10-31 17:20:55 +0100

    a

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import os
 
-S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit"
-
-y = os.system(S)
-print("--------------------------")
-print(type(y))
-
-for x in y:
-    print(x)
+if os.path.isfile("test.fa"):
+    pass
+else:
+    S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit > test.fa"
+    y = os.system(S)
+    print("--------------------------")
+    print(type(y))
