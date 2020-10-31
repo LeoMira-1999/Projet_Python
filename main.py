9ef26982ebcbc0e49ef3ec52f8f3f8b9406598aa
savabarde06 <you@example.com>
2020-10-31 17:27:23 +0100

    a

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from Bio.Blast import NCBIWWW
-help(NCBIWWW.qblast)
-fasta_string1 = open("GCF_001402945.1_ASM140294v1_genomic.fna").read()
-fasta_string2 = open("GCF_001402935.1_ASM140293v1_genomic.fna").read()
-result_handle = NCBIWWW.qblast("blastn", database = "nr"  ,sequence=fasta_string1)
+import pandas as pd
+import os
+
+if os.path.isfile("test.fa"):
+    pass
+else:
+    S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit > test.fa"
+    y = os.system(S)
+    print("--------------------------")
+    print(type(y))
+
+df = pd.read_csv("test.fa", sep='\t', lineterminator='\r')
+
+print(df)
