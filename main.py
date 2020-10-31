f997398023c50306eb95ef67430907c8fa00ba67
savabarde06 <you@example.com>
2020-10-31 16:28:45 +0100

    a

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ help(NCBIWWW.qblast)
 fasta_string1 = open("GCF_001402945.1_ASM140294v1_genomic.fna").read()
 fasta_string2 = open("GCF_001402935.1_ASM140293v1_genomic.fna").read()
 result_handle = NCBIWWW.qblast("blastn", database = "nr"  ,sequence=fasta_string1)
-
-atom://teletype/portal/d111786f-a868-4a3e-a5f9-cd9eafb2f764
---
+++ main_leonardo.py
@@ -0,0 +1,5 @@
+from Bio.Blast import NCBIWWW
+help(NCBIWWW.qblast)
+fasta_string1 = open("GCF_001402945.1_ASM140294v1_genomic.fna").read()
+fasta_string2 = open("GCF_001402935.1_ASM140293v1_genomic.fna").read()
+result_handle = NCBIWWW.qblast("blastn", database = "nr"  ,sequence=fasta_string1)
