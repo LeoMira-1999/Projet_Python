d1699fa0420b0fadac73651d9958161a28233fac
savabarde06 <you@example.com>
2020-10-31 19:34:42 +0100

    a

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pandas as pd
+import pickle
 import os
 
-if os.path.isfile("test.fa"):
-    pass
-else:
-    S = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 6 -subject_besthit > test.fa"
-    y = os.system(S)
-    print("--------------------------")
-    print(type(y))
 
-df = pd.read_csv("test.fa", sep='\t')
+firstCline = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 7 -subject_besthit > blast_raw_1vs2.fa"
+execute = os.system(firstCline)
+
+secondCline = "touch temp_file.txt | echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_1vs2.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_1vs2.fa"
+execute = os.system(secondCline)
+
+thirdCline = "cat temp_file.txt blast_best_hits_1vs2.fa > header_blast.fa | rm temp_file.txt "
+execute = os.system(thirdCline)
+
+df = pd.read_csv("header_blast.fa", sep='\t')
 
 print(df)
+column_ids_seq2 = df['subject acc.ver']
 
-atom://teletype/portal/d111786f-a868-4a3e-a5f9-cd9eafb2f764
+print(column_ids_seq2)
+
+column_ids_seq2.to_csv('SP2.ids', sep='\t')
