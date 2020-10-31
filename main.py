1246bc1da1c738df53c9609b07b9c1e2e9a360f8
savabarde06 <you@example.com>
2020-11-01 00:01:06 +0100

    aaa

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-atom://teletype/portal/79db4b9c-69f4-4a9d-8106-d4aabee43e31
-
 import pandas as pd
 import os
 
-SP1 = ""
+SP2 = "bact1.fna"
 
-SP2 = ""
+SP1 = "bact2.fna"
 
 blastn_SP1_vs_SP2 = "blastn -query "+SP1+" -subject "+SP2+" -outfmt 7 -subject_besthit > blast_raw_1vs2.fa"
 os.system(blastn_SP1_vs_SP2)
@@ -36,7 +34,7 @@ os.system(SP2_DB_creation)
 SP2_blast_hits_seq_query = "blastdbcmd -entry_batch SP2.ids -db SP2/SP2 -out SP2_seq_best_hits_blast_1vs2.fa"
 os.system(SP2_blast_hits_seq_query)
 
-blastn_SP2_best_hits_vs_SP1 = "blastn -query SP2_seq_best_hits_blast_1vs2.fa -subject GCA_004765815.2_ASM476581v2_genomic.fna -outfmt 7 -subject_besthit > blast_raw_2BH_vs_1.fa"
+blastn_SP2_best_hits_vs_SP1 = "blastn -query SP2_seq_best_hits_blast_1vs2.fa -subject "+SP1+" -outfmt 7 -subject_besthit > blast_raw_2BH_vs_1.fa"
 os.system(blastn_SP2_best_hits_vs_SP1)
 
 processing_1 = "echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_2BH_vs_1.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_2vs1.fa"
