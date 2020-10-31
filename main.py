f3517bdc776832eb5aab84ec460dfac5cc99d65f
savabarde06 <you@example.com>
2020-10-31 22:50:36 +0100

    Latest

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import os
 blastn_SP1_vs_SP2 = "blastn -query GCF_001402945.1_ASM140294v1_genomic.fna -subject GCF_001402935.1_ASM140293v1_genomic.fna -outfmt 7 -subject_besthit > blast_raw_1vs2.fa"
 os.system(blastn_SP1_vs_SP2)
 
-processing_1 = "touch temp_file.txt | echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_1vs2.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_1vs2.fa"
+processing_1 = "echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_1vs2.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_1vs2.fa"
 os.system(processing_1)
 
-processing_2 = "cat temp_file.txt blast_best_hits_1vs2.fa > header_blast.fa"
+processing_2 = "cat temp_file.txt blast_best_hits_1vs2.fa > header_blast_1vs2.fa"
 os.system(processing_2)
 
-df = pd.read_csv("header_blast.fa", sep='\t')
+df = pd.read_csv("header_blast_1vs2.fa", sep='\t')
 
 print(df)
 column_ids_seq2 = df['subject acc.ver']
@@ -33,13 +33,13 @@ os.system(SP2_blast_hits_seq_query)
 blastn_SP2_best_hits_vs_SP1 = "blastn -query SP2_seq_best_hits_blast_1vs2.fa -subject GCF_001402945.1_ASM140294v1_genomic.fna -outfmt 7 -subject_besthit > blast_raw_2BH_vs_1.fa"
 os.system(blastn_SP2_best_hits_vs_SP1)
 
-processing_1 = "rm temp_file.txt | touch temp_file.txt | echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_1vs2.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_1vs2.fa"
+processing_1 = "echo \"query acc.ver\tsubject acc.ver\t% identity\talignment length\tmismatches\tgap opens\tq. start\tq. end\ts. start\ts. end\tevalue\tbit score\" > temp_file.txt | cat blast_raw_2BH_vs_1.fa |awk '/hits found/{getline;print}' | grep -v \"#\" > blast_best_hits_2vs1.fa"
 os.system(processing_1)
 
-processing_2 = "cat temp_file.txt blast_best_hits_1vs2.fa > header_blast.fa"
+processing_2 = "cat temp_file.txt blast_best_hits_2vs1.fa > header_blast_2vs1.fa | rm temp_file.txt"
 os.system(processing_2)
 
-df = pd.read_csv("header_blast.fa", sep='\t')
+df = pd.read_csv("header_blast_2vs1.fa", sep='\t')
 
 print(df)
 column_ids_seq2 = df['subject acc.ver']
@@ -47,8 +47,8 @@ column_ids_seq2.reset_index(drop=True, inplace=True)
 
 print(column_ids_seq2)
 
-if os.path.isfile("SP2.ids"):
+if os.path.isfile("reciprocal.ids"):
     pass
 else:
-    with open('SP2.ids', 'x') as f:
+    with open('reciprocal.ids', 'x') as f:
         column_ids_seq2.to_csv(f, sep='\t',header=False, index=False)
