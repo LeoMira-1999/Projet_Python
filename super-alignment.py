6ef575d8a3aecbd783e4023339286696192f2600
savabarde06 <you@example.com>
2020-11-13 11:20:33 +0100

    azeaz

--- super-alignment.py
+++ super-alignment.py
@@ -11,27 +11,54 @@ def gap_filer():
         names = cleaned_names[0].split("-")
         proteome_sp_real.append(names[0])
 
-    for file in glob.glob("test_align/*"):
-        cleaned_file = file.split("test_align/")
-        with open("test_align/"+cleaned_file[1], "r") as cluster_file:
+    for file in glob.glob("aligned_clusters/*"):
+        cleaned_file = file.split("aligned_clusters/")
+
+        with open("aligned_clusters/"+cleaned_file[1], "r") as cluster_file:
             lines = cluster_file.readlines()
             lines = [w.replace('\n', '') for w in lines]
 
             list_sp_file = []
             seq = []
-            for line in lines:
-                if line.startswith(">"):
-                    raw_species = line.split("[")
+            i = 0
+            while i < len(lines):
+                if lines[i].startswith(">"):
+                    raw_species = lines[i].split("[")
                     species = raw_species[-1].split("]")
                     first_name = species[0].split(" ")
                     list_sp_file.append(first_name[0])
+                i+=1
+                one_seq= []
 
-                if line.startswith(">") == False:
-                    seq.append(line)
-            with open("processed_"+cleaned_file[1], "a") as processed_cluster:
-                processed_cluster.write(">")
-            print(list_sp_file)
-            print("".join(seq))
+                while i < len(lines):
+
+                    if lines[i].startswith(">"):
+                        break
+                    else:
+                        one_seq.append(lines[i])
+
+                    i+=1
+                seq.append("".join(one_seq))
+
+
+            with open("aligned_clusters/processed_"+cleaned_file[1], "a") as processed_cluster:
+                missing_sp = list(set(proteome_sp_real)-set(list_sp_file))
+
+                if len(missing_sp) == 0:
+
+                    for sp, sequence in zip(list_sp_file,seq):
+
+                        processed_cluster.write(">"+sp+"\n"+sequence+"\n")
+                if len(missing_sp) > 0:
+                    seq_len = len(seq[0])
+                    for sp, sequence in zip(list_sp_file,seq):
+
+                        processed_cluster.write(">"+sp+"\n"+sequence+"\n")
+
+                    for sp_missing in missing_sp:
+
+                        processed_cluster.write(">"+sp_missing+"\n"+seq_len*"-"+"\n")
+
 
 
 
@@ -41,8 +68,9 @@ def cluster_reader():
     clusters_dict = {}
     species = []
     total_sp = []
-    for file in glob.glob("aligned_clusters/*"):
+    for file in glob.glob("aligned_clusters/processed_*"):
         cleaned_file = file.split("aligned_clusters/")
+        print(cleaned_file)
         with open("aligned_clusters/"+cleaned_file[1], "r") as cluster_file:
             lines = cluster_file.readlines()
             lines = [w.replace('\n', '') for w in lines]
@@ -50,12 +78,10 @@ def cluster_reader():
             temporary_sp = []
             for line in lines:
                 if line.startswith(">"):
-                    raw_species = line.split("[")
-                    species = raw_species[-1].split("]")
-                    first_name = species[0].split(" ")
+                    first_name = line.split(">")
 
-                    temporary_sp.append(first_name[0])
-                    total_sp.append(first_name[0])
+                    temporary_sp.append(first_name[1])
+                    total_sp.append(first_name[1])
 
                 if line.startswith(">") == False:
 
@@ -94,11 +120,16 @@ def cluster_reader():
     for sp in proteome_sp_real:
         seqs.clear()
         seqs.append("".join(clusters_dict[sp]))
+
         cleaned_clusters_dict[sp]=seqs
     print(reprlib.repr(cleaned_clusters_dict))
 
+    with open("final_super_alignment.fa" , "a") as file:
+        for SP, seq in cleaned_clusters_dict.items():
+            for sequence in seq:
+                file.write(">"+SP+"\n"+sequence+"\n")
 
 
-"""cluster_reader()"""
+cluster_reader()
 
-gap_filer()
+"""gap_filer()"""
